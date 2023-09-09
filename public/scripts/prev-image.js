5dca2e0f825d6ba2783ab208989e3e6b7b9009ad
mahamadou-simpara <msimpara@doninya.com>
2023-09-09 18:24:47 +0000

    prev-image func added

--- public/scripts/prev-image.js
+++ public/scripts/prev-image.js
@@ -1,7 +1,21 @@
 const inputEl = document.getElementById('image');
+const img = document.getElementById('img');
+
 
 function displayImagePreview() {
-    console.log('Okay!');
+
+    const files = inputEl.files;
+
+    if(!files || files.length === 0){
+        img.style.display = 'none';
+        return;
+    }
+
+    const pickedFile = files[0];
+    const url = URL.createObjectURL(pickedFile);
+    img.style.display = 'inline';
+    img.src = url;
+
 }
 
-inputEl.addEventListener('upload', displayImagePreview);
+inputEl.addEventListener('change', displayImagePreview);
